43ddc2a00f54bdf1083c54df143b35201ad623b8
imanjra <ibrahim@voxel51.com>
2026-04-27 14:20:20 -0400

    fix height and width not working for ImageView

--- app/packages/core/src/plugins/SchemaIO/components/ImageView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/ImageView.tsx
@@ -1,10 +1,9 @@
-import { Box } from "@mui/material";
-import React from "react";
-import HeaderView from "./HeaderView";
-import { getComponentProps } from "../utils";
-import { usePanelId } from "@fiftyone/spaces";
 import { usePanelEvent } from "@fiftyone/operators";
 import { OperatorResult } from "@fiftyone/operators/src/operators";
+import { usePanelId } from "@fiftyone/spaces";
+import { Box } from "@mui/material";
+import { getComponentProps, parseSize } from "../utils";
+import HeaderView from "./HeaderView";
 
 export default function ImageView(props) {
   const { schema, data } = props;
@@ -16,49 +15,51 @@ export default function ImageView(props) {
     operator,
     prompt = false,
     params,
-    cursor = false,
   } = schema?.view || {};
   const imageURI = data ?? schema?.default;
 
   const panelId = usePanelId();
   const handleClick = usePanelEvent();
+  const isClickable = operator || href;
 
-  const openLink = () => href && window.open(href, "_blank");
+  const onClick = isClickable
+    ? () => {
+        if (operator) {
+          handleClick(panelId, {
+            params,
+            operator,
+            prompt,
+            callback: (result: OperatorResult) => {
+              // execution after operator
 
-  const onClick = () => {
-    if (operator) {
-      handleClick(panelId, {
-        params: params,
-        operator,
-        prompt,
-        callback: (result: OperatorResult) => {
-          // execution after operator
+              if (result?.error) {
+                console.log(result?.error);
+                console.log(result?.errorMessage);
+              } else {
+                openLink();
+              }
+            },
+          });
+        } else if (href) {
+          window.open(href, "_blank");
+        }
+      }
+    : undefined;
 
-          if (result?.error) {
-            console.log(result?.error);
-            console.log(result?.errorMessage);
-          } else {
-            openLink();
-          }
-        },
-      });
-    } else {
-      // execute if operator not defined
-      openLink();
-    }
+  const imageStyles = {
+    cursor: isClickable ? "pointer" : "default",
+    height: parseSize(height, undefined, "px"),
+    width: parseSize(width, undefined, "px"),
   };
 
   return (
     <Box {...getComponentProps(props, "container")}>
       <HeaderView {...props} nested />
       <img
+        {...getComponentProps(props, "image", { style: imageStyles })}
         src={imageURI}
-        height={height}
-        width={width}
         alt={alt}
         onClick={onClick}
-        style={{ cursor: cursor ? "pointer" : "default" }} // Change cursor based on href
-        {...getComponentProps(props, "image")}
       />
     </Box>
   );
